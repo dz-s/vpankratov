239989b46cba6d349ef3a4ac124a9d49b72a620d
dz-s <shulhindvst@gmail.com>
2019-11-16 01:24:38 +0100

    upd with plot

--- relate_avg_bg,py/main.py
+++ relate_avg_bg,py/main.py
@@ -1,28 +1,24 @@
 import pandas as pd
-
-
-def getAVGLength(filepath):
-    total_avg = 0
-    chunk_size = 100
-    reader = pd.read_csv(filepath, iterator=True)
-    step = 0
-    chunk = reader.get_chunk(3)
+import matplotlib.pyplot as plt
     
-    while step < 5:
-        step += 500
-        chunk = reader.get_chunk(3)
-        if total_avg == 0:
-            total_avg = chunk['BP'].mean()
-        else:
-            total_avg = (total_avg + chunk['BP'].mean()) / 2
-        print('Step:', step)
-        print(total_avg, '\n')
+def getAVGLength(filepath):
+    max_bp_per_window = []
+    chunk_size = 10000
+    reader = pd.read_csv(filepath, iterator=True)
+    step = 500
+    i = 0
+    while i < 1000:
+        try:
+            chunk = reader.get_chunk(chunk_size)
+            reader.get_chunk(step) # skip step size
+            max_bp_per_window.append(chunk['BP'].max())
+            i = i + 1
+        except:
+            return list(range(i)), max_bp_per_window
+      
        
 
- 
-
-
-
-
 if __name__ == "__main__":
-    getAVGLength("CEU.csv")
+    x, y = getAVGLength("CEU.csv")
+    hl, = plt.plot(x, [x / 100000 for x in y])
+    plt.show()
