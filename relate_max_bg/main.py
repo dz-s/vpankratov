621216ecbfc2ca10b60efb3c5636a2259958de21
dz-s <shulhindvst@gmail.com>
2019-11-16 01:27:57 +0100

    upd func name

--- relate_max_bg/main.py
+++ relate_max_bg/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
     
-def getAVGLength(filepath):
+def getMAXLength(filepath):
     max_bp_per_window = []
     chunk_size = 10000
     reader = pd.read_csv(filepath, iterator=True)
@@ -19,6 +19,6 @@ def getAVGLength(filepath):
        
 
 if __name__ == "__main__":
-    x, y = getAVGLength("CEU.csv")
+    x, y = getMAXLength("CEU.csv")
     hl, = plt.plot(x, [x / 100000 for x in y])
     plt.show()
